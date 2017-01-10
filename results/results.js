6e74ca9eb5a22ebac45c8a6993a411728672daa4
Shinyi Chen <jchen@isi.edu>
2017-01-09 18:06:14 -0800

    Paging

--- results/results.js
+++ results/results.js
@@ -23,17 +23,35 @@
 
             $scope.clusters = null;
 
+            $scope.counter = 0;
+
             $scope.cluster_id = null;
 
             $scope.showOpener = true;
 
             $scope.showCluster = false;
 
+            $scope.lastPage = 0;
+
+            $scope.page = {};
+
+
+
+            var clusterIDs = [];
+
+            var pageSize = 25;
+
             $scope.open = function() {
 
                 // csv file
                 $scope.clusters = FileUtils.csv2json($scope.text);
-
+                clusterIDs = Object.keys($scope.clusters);
+                $scope.count = clusterIDs.length;
+                $scope.lastPage = Math.ceil(clusterIDs.length / pageSize);
+                $scope.page = {
+                    "number": 1,
+                    "clusters": clusterIDs.slice(0, Math.min(clusterIDs.length, pageSize))
+                };
                 $scope.showOpener = false;
             };
 
@@ -64,6 +82,23 @@
             $scope.restart = function() {
                 $scope.showOpener = true;
             };
+
+
+
+            $scope.previous = function() {
+                if ($scope.page.number !== 1) { // starts at 1
+                    $scope.page.number -= 1;
+                    $scope.page.clusters = clusterIDs.slice(($scope.page.number - 1) * pageSize, Math.min(clusterIDs.length, $scope.page.number * pageSize));
+                }
+
+            };
+
+            $scope.next = function() {
+                if ($scope.page.number !== $scope.lastPage) {
+                    $scope.page.number += 1;
+                    $scope.page.clusters = clusterIDs.slice(($scope.page.number - 1) * pageSize, Math.min(clusterIDs.length, $scope.page.number * pageSize));
+                }
+            }
 
         }])
 
