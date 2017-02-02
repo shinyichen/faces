f6356f260968d90593d4c6927db98ff6d3e0f281
Shinyi Chen <jchen@isi.edu>
2017-02-02 13:58:48 -0800

    Color and labels

--- analysis/analysis.js
+++ analysis/analysis.js
@@ -8,20 +8,45 @@
 
         .run(['$rootScope', '$http', function($rootScope, $http) {
 
-            $rootScope.data = [];
+            $rootScope.dataset = [];
 
             $rootScope.data_ready = false;
 
 
             var data_file = "../data/analysis/2d.txt";
 
+            var label_file = "../data/analysis/labels.txt";
+
+            var data, labels;
+
             $http.get(data_file).then(function(response) {
-                $rootScope.data = parse(response.data);
+                // parse array
+                data = parse2D(response.data);
+
+                return $http.get(label_file);
+
+            }, function(error) {
+                // parse 2d.txt failed
+            }).then(function(response) {
+                // parse labels
+                labels = parseLabels(response.data);
+
+                // combine data and label and create dataset
+                for (var r = 0; r < data.length; r++) { // rows
+                    $rootScope.dataset[r] = {
+                        "x": data[r][0],
+                        "y": data[r][1],
+                        "label": labels[r]
+                    }
+                }
+
+                // allow drawing to begin
                 $rootScope.data_ready = true;
             }, function(error) {
+                // parse labels.txt failed
             });
 
-            function parse(csv) {
+            function parse2D(csv) {
 
                 var data = [];
 
@@ -45,29 +70,60 @@
 
                 return data;
             }
+
+            function parseLabels(csv) {
+
+                var labels = [];
+
+                var lines=csv.split("\n");
+
+                for (var i = 0; i < lines.length; i++){
+                    labels.push(lines[i]);
+                }
+
+                return labels;
+            }
         }])
 
         .directive('plot', ['d3Service', function (d3Service) {
             return {
                 restrict: 'E',
                 scope: {
-                    dataset: "="
+                    dataset: "=",
+                    labels: "="
                 },
                 link: function(scope, element, attrs) {
 
                     d3Service.d3().then(function(d3) {
+
+                        var colors = {};
+
                         var w = 1000, h = 700, padding = 20, transform = d3.zoomIdentity;
 
                         // scale functions to make data fit in the viewport
                         var xScale = d3.scaleLinear()
-                            .domain([d3.min(scope.dataset, function(d) { return d[0]; }),
-                                     d3.max(scope.dataset, function(d) { return d[0]; })])
+                            .domain([d3.min(scope.dataset, function(d) { return d.x; }),
+                                     d3.max(scope.dataset, function(d) { return d.x; })])
                             .range([padding, w - padding * 2]);
 
                         var yScale = d3.scaleLinear()
-                            .domain([d3.min(scope.dataset, function(d) { return d[1]; }),
-                                     d3.max(scope.dataset, function(d) { return d[1]; })])
+                            .domain([d3.min(scope.dataset, function(d) { return d.y; }),
+                                     d3.max(scope.dataset, function(d) { return d.y; })])
                             .range([h - padding, padding]);
+
+                        // color function (label->color)
+                        var letters = '0123456789ABCDEF';
+                        function getColor(label) { // label is string of integer
+                            if (!colors[label]) {
+                                var color = '#';
+                                for (var i = 0; i < 6; i++ ) {
+                                    color += letters[Math.floor(Math.random() * 16)];
+                                }
+                                colors[label] = color
+                            }
+
+                            return colors[label];
+                        }
 
                         var svg = d3.select(element[0]).append("svg");
                         svg.attr("width", w)
@@ -90,14 +146,15 @@
                             .data(scope.dataset)
                             .enter().append("circle")
                             .attr("cx", function(d) {
-                                return xScale(d[0]);
+                                return xScale(d.x);
                             })
                             .attr("cy", function(d) {
-                                return yScale(d[1]);
+                                return yScale(d.y);
                             })
                             .attr("r", 1)
-                            .attr('fill', 'rgb(100,100,255)')
-                            //.attr("transform", "translate(" + w/2 + "," + h/2 + ")") // center
+                            .attr('fill', function(d) {
+                                return getColor(d.label);
+                            })
                             .call(d3.drag().on("drag", dragged));
 
                         svg.call(d3.zoom()
